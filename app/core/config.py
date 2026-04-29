8bd9a5b7d027b3542da77011141e1422c2a4ef6d
Manikandan <manikandan.g@technorucs.com>
2026-04-29 23:28:18 +0530

    feat: implement authentication core including configuration, JWT security utilities, auth schemas, and endpoint scaffold

--- app/core/config.py
+++ app/core/config.py
@@ -27,7 +27,9 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str = Field(alias="SECRET_KEY")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 480
+    #30 minutes expiry time
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     BACKEND_CORS_ORIGINS: Union[list[str], str] = Field(default=[])
 
